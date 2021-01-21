076cab63eb666b09956642deffff80097413e901
totti <253262275@qq.com>
2021-01-22 07:50:07 +0800

    添加hook，删除临时文件

--- java-cdp/src/main/java/totti/RunChrome.java
+++ java-cdp/src/main/java/totti/RunChrome.java
@@ -1,11 +1,8 @@
 package totti;
 
-import utils.FlagUtils;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -15,36 +12,43 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import utils.FileUtils;
+import utils.FlagUtils;
+
 public class RunChrome {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String tempDir = "/home/C5311429/temp/chromedb-runner" + new Random().nextInt(1000);
+        // String tempDir = "/home/C5311429/temp/chromedb-runner" + new Random().nextInt(1000);
+        String tempDir = "/Users/totti/temp/chromedb-runner" + new Random().nextInt(1000);
         File file = new File(tempDir);
         if (!file.exists()) {
             file.mkdir();
         }
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            file.delete();
+            FileUtils.deleteDir(file);
         }));
 
         String methodName = "Page.navigate";
-        String path = "/usr/bin/google-chrome";
-        // String macPath = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+        // String path = "/usr/bin/google-chrome";
+        String path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
         // {"id":14,"sessionId":"7DD78405FDAAD20E11D3B02EB0293B50","method":"Page.navigate","params":{"url":"http://www.baidu.com"}}
         // ws://127.0.0.1:51574/devtools/browser/d3abe4e8-4644-4252-9a8e-d620fc03a0a3
 
-//        File.createTempFile("chromedb-runner123", "");
+        // File.createTempFile("chromedb-runner123", "");
 
         List<String> cmd = new ArrayList<>();
         cmd.add(path);
         cmd.addAll(Arrays.stream(FlagUtils.getFlags(tempDir)).collect(Collectors.toList()));
         ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+        processBuilder.redirectInput();
 
         processBuilder.start();
 
         new Thread(() -> {
             try {
-                FileInputStream fileInputStream = new FileInputStream(file.getPath() + File.separator + "DevToolsActivePort");
+                TimeUnit.SECONDS.sleep(5);
+                FileInputStream fileInputStream =
+                    new FileInputStream(file.getPath() + File.separator + "DevToolsActivePort");
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
                 String line = "";
                 int count = 0;
@@ -55,7 +59,7 @@ public class RunChrome {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        });
+        }).start();
 
         TimeUnit.SECONDS.sleep(10);
     }
---
+++ java-cdp/src/main/java/utils/FileUtils.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.io.File;
+
+public class FileUtils {
+    private FileUtils() {}
+
+    public static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        return dir.delete();
+    }
+}
